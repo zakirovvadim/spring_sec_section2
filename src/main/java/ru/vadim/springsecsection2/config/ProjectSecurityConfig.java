2fe27c47156bdcf8003b844590997db2e7f6832a
VadimZakirov <appolon17@gmail.com>
2025-10-13 23:29:50 +0500

    Example with JdbcUserDetailsManager

--- src/main/java/ru/vadim/springsecsection2/config/ProjectSecurityConfig.java
+++ src/main/java/ru/vadim/springsecsection2/config/ProjectSecurityConfig.java
@@ -1,20 +1,17 @@
 package ru.vadim.springsecsection2.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -32,11 +29,16 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
+//        UserDetails admin = User.withUsername("admin").password("$2a$12$NUSBnw/flPI5Hogk752fY.sSRFayWRiCIvgN90K3LLN7Oywl5q90K").authorities("admin").build();
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
+
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("$2a$12$NUSBnw/flPI5Hogk752fY.sSRFayWRiCIvgN90K3LLN7Oywl5q90K").authorities("admin").build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public UserDetailsService userDetailsService(DataSource ds) {
+      return new JdbcUserDetailsManager(ds);
     }
 
     @Bean
